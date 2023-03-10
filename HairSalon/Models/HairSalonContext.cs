41b8fa9f3527dd22658cc27ce500e8ca92583250
BrentHickman <brenthickman@ymail.com>
2023-03-10 09:48:23 -0800

    homepage and models created

---
+++ HairSalon/Models/HairSalonContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon.Models
+{
+  public class HairSalonContext : DbContext
+  {
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Stylist> Categories { get; set; }
+
+    public HairSalonContext(DbContextOptions options) : base(options) { }
+  }
+}
